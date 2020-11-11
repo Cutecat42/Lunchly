4f81c8a0c40d25a8302b6f59c3aaced0daba51ca
Cat Meza <musical_kitty@hotmail.com>
2020-11-11 13:47:42 -0700

    initial commit with starter code

---
+++ models/reservation.js
@@ -0,0 +1,41 @@
+const moment = require("moment");
+
+const db = require("../db");
+
+
+
+class Reservation {
+  constructor({id, customerId, numGuests, startAt, notes}) {
+    this.id = id;
+    this.customerId = customerId;
+    this.numGuests = numGuests;
+    this.startAt = startAt;
+    this.notes = notes;
+  }
+
+
+
+  getformattedStartAt() {
+    return moment(this.startAt).format('MMMM Do YYYY, h:mm a');
+  }
+
+
+
+  static async getReservationsForCustomer(customerId) {
+    const results = await db.query(
+          `SELECT id, 
+           customer_id AS "customerId", 
+           num_guests AS "numGuests", 
+           start_at AS "startAt", 
+           notes AS "notes"
+         FROM reservations 
+         WHERE customer_id = $1`,
+        [customerId]
+    );
+
+    return results.rows.map(row => new Reservation(row));
+  }
+}
+
+
+module.exports = Reservation;
